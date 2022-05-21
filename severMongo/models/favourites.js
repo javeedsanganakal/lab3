ad95c51eae70ea474fa0768f69bd8a558ba8409c
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-05-21 23:34:04 +0530

    Update favourites.js

--- severMongo/models/favourites.js
+++ severMongo/models/favourites.js
@@ -2,17 +2,19 @@ const mongoose = require("mongoose");
 const Userdb = require("./model");
 const Items = require("./items");
 
-var schema = new mongoose.Schema({
+var favschema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
+    require:true,
     ref: Userdb,
   },
   itemId: {
     type: mongoose.Schema.Types.ObjectId,
+    require:true,
     ref: Items,
   },
 });
 
-const Favourites = mongoose.model("Favourites", schema);
+const Favourites = mongoose.model("Favourites", favschema);
 
 module.exports = Favourites;
