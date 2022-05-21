66363cad1aa2d7cc7c6630536db17f1d6d55b198
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-05-21 15:42:08 +0530

    commit -m "index"

--- severMongo/routes/index.js
+++ severMongo/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello");
+router.get("/", function(req, res) {
+  res.send("Welcome");
 });
