927b9ee23b929e441c56afc9b634ec8d79547884
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-05-21 15:38:56 +0530

    commit -m "model"

--- severMongo/models/model.js
+++ severMongo/models/model.js
@@ -3,37 +3,46 @@ const mongoose = require("mongoose");
 var schema = new mongoose.Schema({
   username: {
     type: String,
+    require:true,
     default: null,
   },
   email: {
     type: String,
+    require:true,
     default: null,
   },
   password: {
     type: String,
+    require:true,
     default: null,
   },
   fullAddress: {
     type: String,
+    require:true,
     default: null,
   },
   city: {
     type: String,
+    require:true,
     default: null,
   },
   phoneNumber: {
     type: String,
+    require:true,
   },
   dob: {
     type: String,
+    require:true,
     default: null,
   },
   gender: {
     type: String,
+    require:true,
     default: null,
   },
   profilePic: {
     type: String,
+    require:true,
     default: null,
   },
   about: {
