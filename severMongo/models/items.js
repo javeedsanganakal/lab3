f40e9145cc0023abd843ffe18b3e80d81d7da71f
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-05-21 15:37:29 +0530

    commit -m "item"

--- severMongo/models/items.js
+++ severMongo/models/items.js
@@ -8,30 +8,37 @@ var schema = new mongoose.Schema({
   },
   itemName: {
     type: String,
+    required:true,
     default: null,
   },
   itemCategory: {
     type: String,
+    required:true,
     default: null,
   },
   itemPrice: {
     type: Number,
+     required:true,
     default: null,
   },
   itemDescription: {
     type: String,
+    required:true,
     default: null,
   },
   itemCount: {
     type: Number,
+    required:true,
     default: null,
   },
   itemImage: {
     type: String,
+    required:true,
     default: null,
   },
   sales: {
     type: Number,
+    required:true,
     default: 0,
   },
 });
